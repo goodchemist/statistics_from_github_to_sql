ad48d15fbf9c66cc52f1e7242174ba26a98edf2f
good_chemist <iolgasolovyeva@gmail.com>
2024-04-01 15:37:53 +0700

    improve function 'get_repos_stats'

--- functions.py
+++ functions.py
@@ -8,13 +8,13 @@ def get_repos_stats(user_name) -> list[dict]:
     :return: список словарей, содержащих статистику по каждому репозиторию
     """
     response = requests.get(f'https://api.github.com/users/{user_name}/repos')
-    print(response)
+
     response_json = response.json()
 
     data = []
 
     for response in response_json:
-        data_dict = {'name': response['name'], 'url': response['url'], 'description': response['description'],
+        data_dict = {'username': response['name'], 'url': response['url'], 'description': response['description'],
                      'language': response['language'], 'watchers': response['watchers'],
                      'forks_count': response['forks_count']}
         data.append(data_dict)
